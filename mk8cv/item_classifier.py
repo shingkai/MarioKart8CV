1bf488b4b6efeb7b25078a77687a307c7d9b383d
Mike Chiu <shingkai.chiu@gmail.com>
2024-10-17 18:45:48 -0700

    swapping item classifier to mobilenetv3

--- mk8cv/item_classifier.py
+++ mk8cv/item_classifier.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from torchvision import models, transforms
 import cv2
 from cv2.typing import MatLike
@@ -34,26 +35,12 @@ classes = ['01', '02', '03', '04', '05', '07', '09', '10', '11', '12', '13', '15
 # # Check if GPU is available (either cuda for nvidia or mps for apple silicon)
 device = torch.device("cuda:0" if torch.cuda.is_available() else torch.device("mps") if torch.backends.mps.is_available() else  torch.device("cpu"))
 
-# def load_model(model_path: str):
-#     # Load the model architecture
-#     model = models.squeezenet1_1(pretrained=False)
-#      # Adjust final layer to match output classes
-#     model.fc = torch.nn.Linear(model.fc.in_features, len(class_names))
-#     # Load the saved weights
-#     model.load_state_dict(torch.load(model_path, map_location=device))
-#     # Set the model to evaluation mode
-#     model.eval()
-#     # Move the model to the appropriate device (MPS or CPU)
-#     model = model.to(device)
-#
-#     return model
 
 def load_model(model_path):
-    model = models.squeezenet1_1(pretrained=False)
+    model = models.mobilenet_v3_large(pretrained=False)
     # Modify the classifier to match your number of classes
     num_classes = len(classes)  # Replace with the number of classes in your dataset
-    model.classifier[1] = torch.nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
-    model.num_classes = num_classes
+    model.classifier[3] = nn.Linear(model.classifier[3].in_features, num_classes)
 
     # Load the saved state dict
     model.load_state_dict(torch.load(model_path, map_location=device))
@@ -61,31 +48,17 @@ def load_model(model_path):
     model.eval()
     return model
 
-model = load_model('./models/item_classifier_squeezenet.pth')
-# model = load_model('/home/itsgrimetime/code/MarioKart8CV/notebooks/best_model_vgg16.pth')
+
+model = load_model('./models/mobilenetv3_finetuned.pth')
 
 
-# # Define the transformation to be applied to the input image (same as used during training)
-# preprocess = transforms.Compose([
-#     transforms.ToPILImage(),  # Convert numpy array (cv2) to PIL image
-#     transforms.Resize(256),
-#     transforms.CenterCrop(224),
-#     transforms.ToTensor(),
-#     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-# ])
-img_width, img_height = 112, 112
+img_width, img_height = 96, 96
 preprocess = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((img_width, img_height)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
-
-
-# Function to predict on a single image
-# def predict_image(image_path, model, transform):
-#     image = cv2.imread(image_path)
-#     predict_frame(image, model, transform)
 
 
 # Function to predict on a cv2 frame
@@ -95,33 +68,11 @@ def predict_frame(frame, preprocess=preprocess):
 
     with torch.no_grad():
         output = model(image)
-        probabilities = torch.nn.functional.softmax(output[0], dim=0)
-        predicted_class = torch.argmax(probabilities).item()
-        confidence = probabilities[predicted_class].item()
+        _, predicted = torch.max(output, 1)
 
-    return predicted_class, confidence
+    predicted_class = classes[predicted.item()]
+    return predicted_class
 
-# def predict_image(frame) -> str:
-#     # Preprocess the image
-#     img = preprocess(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-#
-#     # Add batch dimension (PyTorch models expect a batch of images)
-#     img = img.unsqueeze(0)
-#
-#     # Move the image to the same device as the model (MPS or CPU)
-#     img = img.to(device)
-#
-#     # Turn off gradients for inference (faster and reduces memory usage)
-#     with torch.no_grad():
-#         outputs = model(img)
-#
-#     # Get the predicted class (with the highest score)
-#     _, predicted_class = torch.max(outputs, 1)
-#
-#     # Map the predicted class index to the class name
-#     predicted_class_name = class_names[predicted_class.item()]
-#
-#     return predicted_class_name
 
 def extract_player_items(frame: MatLike, player: Player, state: PlayerState) -> PlayerState:
     """Extracts the player's items from the frame."""
@@ -133,6 +84,6 @@ def extract_player_items(frame: MatLike, player: Player, state: PlayerState) -> 
     item2_coords = CROP_COORDS[player][Stat.ITEM2]
     item2 = predict_frame(frame[round(height * item2_coords[2]): round(height * item2_coords[3]), round(width * item2_coords[0]): round(width * item2_coords[1])])
 
-    state.item1 = Item(int(classes[item1[0]]))
-    state.item2 = Item(int(classes[item2[0]]))
+    state.item1 = Item(int(item1))
+    state.item2 = Item(int(item2))
     return state
